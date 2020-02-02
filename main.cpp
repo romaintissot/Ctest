01aa5261ea27f82445170dc86152d471ae0f34e7
Romain <romain.tissot@live.fr>
2020-02-03 00:06:54 +0100

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream> // Inclut la bibliothèque iostream (affichage de texte)
+
+using namespace std; // Indique quel espace de noms on va utiliser
+
+/*
+Fonction principale "main"
+Tous les programmes commencent par la fonction main
+*/
+int main()
+{
+    string name("Mr. Robot");
+    cout << "Hello world!" << endl<<"My name is "<<name<<endl; // Affiche un message
+    return 0; // Termine la fonction main et donc le programme
+}
