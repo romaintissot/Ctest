d67b0f60ed72815e89d546d30761a1c2b1108b33
Romain <romain.tissot@live.fr>
2020-02-05 00:48:45 +0100

    commit melange

--- main.cpp
+++ main.cpp
@@ -1,12 +1,32 @@
 #include <iostream> // Inclut la bibliothèque iostream (affichage de texte)
 #include <vector>
 #include <fstream>
+#include <ctime>
+#include <cstdlib>
 #include "math.h"
 
 
 using namespace std; // Indique quel espace de noms on va utiliser
 
+string mixedWord(string word)
+{
 
+    int wordSize = word.size();
+    int randomNumber = rand() % wordSize;
+    string newWord;
+    while (wordSize != 0)
+    {
+
+            newWord+=word[randomNumber];
+            word.erase(randomNumber,1);
+            wordSize = word.size();
+            randomNumber = rand() % wordSize;
+
+    }
+
+
+    return newWord;
+}
 
 
 
@@ -26,19 +46,29 @@ int main()
     cout<<"What's your number?"<<endl;
     cin>>number;
     cout<<"+2 :"<<ajouteDeux(number)<<endl;*/
+    srand(time(0));
+    string initialWord;
+    string tryWord;
 
     cout << "Hello player 1, please choose a word :"<<endl;
-    cin>>word;
+    cin>>initialWord;
 
-    cout << "Hello player 2, guess the word : "<<mixedWord<<endl;
-    cin>>tryWord;
-
-    while(tryWord == word) do{
-        cout << "wrong, sorry. Try again : "<<mixedWord<<endl;
+    do{
+        cout << "Hello player 2, guess the word : "<<mixedWord(initialWord)<<endl;
         cin>>tryWord;
-    }
+    if (tryWord == initialWord)
+      {
+         cout << "Congrats! you found it :)"<<endl;
+      }
+      else
+      {
+         cout << "wrong, sorry. Try again : "<<mixedWord(initialWord)<<endl;
+      }
 
-    cout << "Congrats! you found it :)"<<endl;
+
+    }while(tryWord != initialWord);
+
+
 
     return 0; // Termine la fonction main et donc le programme
 }
